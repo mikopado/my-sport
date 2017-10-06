feb3f288dcae51f29b81da2d1ddfa50d4c425e5b
michele ercolino <michele.ercolino45@gmail.com>
2017-10-06 16:27:36 +0100

    Create new project and replace old due to issues

---
+++ SportsBarApp/SportsBarApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SportsBarApp.Startup))]
+namespace SportsBarApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
