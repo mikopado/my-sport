4f27ba90392b3693baf3652e4f2ec5801aafa711
michele ercolino <michele.ercolino45@gmail.com>
2018-01-24 14:27:09 +0000

    update date picker to greater range

--- SportsBarApp/Scripts/appScripts/myScript.js
+++ SportsBarApp/Scripts/appScripts/myScript.js
@@ -3,10 +3,10 @@ $(function () {
 
 
     $(".datePicker").datepicker({
-
         dateFormat: "dd/mm/yy",
         changeYear: true,
-        maxDate: '0'
+        maxDate: '0',
+        yearRange: "c-100:c+00"
     });
 
     
